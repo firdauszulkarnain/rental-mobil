2d8becffe13a71dd0b09a38be7b975eab9317fe1
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2022-07-21 19:50:07 +0800

    refactor code + login, logout

---
+++ admin/login.php
@@ -0,0 +1,149 @@
+<?php
+
+session_start();
+if (isset($_SESSION['admin'])) {
+    $url = 'index.php';
+    header("Location: $url");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- SWEETALERT2 -->
+    <script src="../assets/vendor/sweetalert2/sweetalert2.all.min.js"></script>
+    <!-- Custom styles for this template-->
+    <link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-6 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 mb-4 text-dark font-weight-bolder">LOGIN ADMIN</h1>
+                                    </div>
+                                    <hr>
+                                    <form class="user" method="POST" action="">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" placeholder="Username" name="username" autocomplete="off">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                                        </div>
+                                        <button class="btn btn-primary btn-user btn-block" type="submit" name="login">
+                                            Login
+                                        </button>
+                                        <hr>
+                                    </form>
+                                    <div class="text-center">
+                                        <p class="small" href=#">All Right Reserved Rental Mobil</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../assets/vendor/jquery/jquery.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../assets/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
+
+<?php
+
+require '../functions/functions.php';
+if (isset($_POST['login'])) {
+    // ambil data pada form input login menggunakan method post
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+
+    // jika inputan kosong, tampilkan aler
+    if (empty($username) || empty($password)) {
+        echo    "<script>
+                Swal.fire('Erorr','Form Tidak Boleh Kosong','error');
+                </script>";
+        exit;
+    }
+
+    // cek username, apakah ada di database
+    $result = mysqli_query($db, "SELECT * FROM admin WHERE username = '$username'");
+
+    // jika ada 
+    if (mysqli_num_rows($result) == 1) {
+        // ambil datanya
+        $row = mysqli_fetch_assoc($result);
+        // kemudian cek passwordnya sama atau tidak
+        if (password_verify($password, $row['password'])) {
+            // kalo sama 
+            //Set Session user
+            $_SESSION['admin'] = $row['id_admin'];
+            // tampilkan alert berhasil login
+            echo    "<script>
+                Swal.fire('Login Berhasil','','success').then(function() {
+                    window.location = 'index.php';
+                });;
+            </script>";
+        } else {
+            // kalo gak sama, berarti passwordnya salah
+            echo    "<script>
+                Swal.fire('Login Gagal','Password Salah','warning').then(function() {
+                    window.location = 'login.php';
+                });;
+            </script>";
+        }
+    } else {
+        // kalo gak ada di database, berarti tidak ditemukan
+        echo    "<script>
+    Swal.fire('Login Gagal','Username Tidak Ditemukan','error').then(function() {
+        window.location = 'login.php';
+    });;
+    </script>";
+    }
+} else {
+    // digunakan untuk menampilkan informasi error database
+    echo mysqli_error($db);
+}
+
+?>
---
+++ admin/logout.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+// hapuskan semua session
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- SWEETALERT2 -->
+	<script src="../assets/vendor/sweetalert2/sweetalert2.all.min.js"></script>
+	<title>Logout</title>
+</head>
+
+<body>
+
+	<script>
+		Swal.fire('Success', 'Berhasil Logout!', 'success').then(function() {
+			window.location = 'login.php';
+		});
+	</script>
+
+</body>
+
+</html>
